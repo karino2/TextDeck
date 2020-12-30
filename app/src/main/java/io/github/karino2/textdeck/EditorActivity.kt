046ac894f21985088968bdf449d7a850652d30d8
Kazuma Arino <hogeika2@gmail.com>
2020-12-30 21:09:08 +0900

    Add update check for onStart.

--- app/src/main/java/io/github/karino2/textdeck/EditorActivity.kt
+++ app/src/main/java/io/github/karino2/textdeck/EditorActivity.kt
@@ -5,27 +5,79 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import io.github.karino2.listtextview.ListTextView
+import kotlinx.coroutines.*
+import kotlinx.coroutines.withContext
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.util.*
+import kotlin.coroutines.CoroutineContext
 
 
-class EditorActivity : AppCompatActivity() {
+class EditorActivity : AppCompatActivity(), CoroutineScope {
     companion object {
         const val REQUEST_EDIT_CELL_CODE=1
         const val  LAST_URI_KEY = "last_uri_path"
+        const val  LAST_READ_TIME_KEY = "last_read_time"
+        const val SHORT_ENOUGH = 1*60*1000
 
         fun lastUriStr(ctx: Context) = sharedPreferences(ctx).getString(LAST_URI_KEY, null)
         fun writeLastUriStr(ctx: Context, path : String) = sharedPreferences(ctx).edit()
             .putString(LAST_URI_KEY, path)
             .commit()
 
+        fun lastReadTime(ctx: Context) = sharedPreferences(ctx).getLong(LAST_READ_TIME_KEY, 0)
+        fun writeLastReadTime(ctx: Context, time : Long) = sharedPreferences(ctx).edit()
+            .putLong(LAST_READ_TIME_KEY, time)
+            .commit()
+
         private fun sharedPreferences(ctx: Context) = ctx.getSharedPreferences("TEXT_DECK_PREFS", Context.MODE_PRIVATE)
 
         fun showMessage(ctx: Context, msg : String) = Toast.makeText(ctx, msg, Toast.LENGTH_SHORT).show()
     }
+
+    lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    override fun onStart() {
+        job = Job()
+        super.onStart()
+        checkUpdate()
+    }
+
+    private fun checkUpdate() {
+        val now = (Date()).time
+        val last = lastReadTime(this)
+
+        if (last != 0L && (now-last) < SHORT_ENOUGH )
+            return
+
+        lastUriStr(this) ?: return
+
+        writeLastReadTime(this, now)
+        launch(Dispatchers.IO) {
+            val desc = contentResolver.openFileDescriptor(lastUri, "r")!!
+            val fis = FileInputStream(desc.fileDescriptor)
+            val text = fis.bufferedReader().use { it.readText() }
+            if (text == lastReadContent)
+                return@launch
+
+            lastReadContent = text
+            withContext(Dispatchers.Main)
+            {
+                listTextView.text = text
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        job.cancel()
+    }
+
+
 
     val listTextView: ListTextView by lazy {
         findViewById<ListTextView>(R.id.listTextView).apply {
@@ -37,6 +89,8 @@ class EditorActivity : AppCompatActivity() {
 
     val lastUri : Uri
        get() = Uri.parse(lastUriStr(this))!!
+
+    var lastReadContent : String = ""
 
     fun saveFile() {
         val desc = contentResolver.openFileDescriptor(lastUri, "w")!!
@@ -83,9 +137,11 @@ class EditorActivity : AppCompatActivity() {
     }
 
     fun openUri(uri: Uri) {
+        writeLastReadTime(this, (Date()).time)
         val desc = contentResolver.openFileDescriptor(uri, "r")!!
         val fis = FileInputStream(desc.fileDescriptor)
         val text = fis.bufferedReader().use { it.readText() }
         listTextView.text = text
+        lastReadContent = text
     }
 }
